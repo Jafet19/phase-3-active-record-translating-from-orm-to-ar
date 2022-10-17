74d2ad4b939514eaaf4f433ac503f31106bacbf1
Jafet Reyes Jr <jafet_19@Jafets-MacBook-Pro.local>
2022-10-17 09:10:01 -0500

    commit tree 1e2438992ca089bf6b974f91d31da5a553f3c1bf

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,12 @@
-class Dog
+class Dog < ActiveRecord::Base
+
+    attr_accessor :name, :breed
+
+    def initialize(name,breed)
+        @name = name
+        @breed = breed
+        @id = id
+    end
+    Dog.create(name, breed)
 end
+
